63b59336ca1d5fe5c4f44c89b09144ec03a2eecb
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-02-12 01:02:12 +0530

    calculator

---
+++  Kirana_Store_Calculator.py
@@ -0,0 +1,20 @@
+# sum =0
+# for i in range(0,6):
+#     n=int(input("Enter the item price :"))
+#     sum=sum+n
+
+#     print("Total price",'$',sum) 
+sum=0
+while(True):
+    user=input("Enter the item price :")
+    if (user!='q'):
+        sum=sum+int(user)
+        print(f"Order total so far:{sum}")
+    else:
+        print(f"Your Bill total is {sum} thanks for shoping us")    
+        break
+
+   
+
+   
+
