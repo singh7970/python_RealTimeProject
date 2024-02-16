2b727e1a70d9f6f030d69992126b83e1d1b8c8be
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-02-17 01:03:41 +0530

    feb 17

---
+++ password_generator.py
@@ -0,0 +1,7 @@
+n = 7
+import random
+s = "qwertyuiopasdfghjklzxcvbnm1234567890!@#$%&?/"
+for i in range(n):
+    print(random.choice(s), end="")
+print()  # Add a newline character to move to the next line
+
