d69bef5cee98553ed987ea2cbd04c301c1b6eca7
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-02-18 19:33:38 +0530

    update

---  Kirana_Store_Calculator.py
+++  Kirana_Store_Calculator.py
@@ -6,7 +6,7 @@
 #     print("Total price",'$',sum) 
 sum=0
 while(True):
-    user=input("Enter the item price :")
+    user=input("Enter the item price or CLICK 'q' to quit:")
     if (user!='q'):
         sum=sum+int(user)
         print(f"Order total so far:{sum}")
