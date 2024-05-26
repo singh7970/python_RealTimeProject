de0f6c973eef328fdb978ff0a8fa8f78e7bae0a3
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-05-26 23:56:03 +0530

    qrcode

---
+++ QRcode/QR_code.py
@@ -0,0 +1,14 @@
+# import qrcode as qr
+# img=qr.make("")
+# img.save("hello.png")
+
+
+
+
+
+# to pass a html file
+import qrcode as qr
+with open('QRcode/basic_info.txt','r') as file:
+   info=file.read()
+   img=qr.make(info)
+   img.save('hello.png')
